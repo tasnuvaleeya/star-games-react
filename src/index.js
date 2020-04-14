b488fc729b0ec0c6b2c5e10d4239b5613031039e
tasnuvaleeya <tasnuva2606@gmail.com>
2020-04-14 20:06:54 +0600

    Revert "star games: initial commit, a game to play"
    This reverts commit 877050dbd27afaee95d57585ab6158b1c3234dd0.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import StarMatch from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StarMatch />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
